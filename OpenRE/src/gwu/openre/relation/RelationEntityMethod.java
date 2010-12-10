ecf57ae5d6b2dd0f15daffddc0992aa8a24b9b27
zhangyzz <zhangyzz@gwmail.gwu.edu>
2010-12-09 20:05:57 -0500

    version 1.0

---
+++ OpenRE/src/gwu/openre/relation/RelationEntityMethod.java
@@ -0,0 +1,9 @@
+package gwu.openre.relation;
+
+public interface RelationEntityMethod {
+	public String getRelatedName();
+	public String getComName();
+	public int getLine();
+	public int getDocID();
+
+}
